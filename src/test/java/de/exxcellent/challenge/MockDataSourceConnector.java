169fe3008aa8a1f6161d7b200fc6f20e61854692
Stefan <svill33@gmail.com>
2024-12-16 17:23:15 +0100

    feat: added class to mock datasourceconnector for test cases

---
+++ src/test/java/de/exxcellent/challenge/MockDataSourceConnector.java
@@ -0,0 +1,25 @@
+package de.exxcellent.challenge;
+
+import de.exxcellent.challenge.io.IDataSourceConnector;
+
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class MockDataSourceConnector<T> implements IDataSourceConnector<T> {
+
+    private List<Map<String,String>> mockData;
+    public MockDataSourceConnector(List<Map<String,String>> mockData){
+        this.mockData = mockData;
+    }
+    @Override
+    public List<T> readData(Function<Map<String, String>, T> dataMapping) throws FileNotFoundException {
+        List<T> result = new LinkedList<>();
+        for (var element : mockData){
+            result.add(dataMapping.apply(element));
+        }
+        return result;
+    }
+}
