e7ec3c03de59ca10a772c762fb10bf5b1b3f04ed
Stefan <svill33@gmail.com>
2024-12-16 18:27:21 +0100

    feat: add football analyses to main App class

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,9 +1,12 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.analyzer.FootballAnalyzer;
 import de.exxcellent.challenge.analyzer.WeatherAnalyzer;
 import de.exxcellent.challenge.io.CSVSourceConnector;
+import de.exxcellent.challenge.io.FootballReader;
 import de.exxcellent.challenge.io.IDataReader;
 import de.exxcellent.challenge.io.WeatherReader;
+import de.exxcellent.challenge.model.FootballDataEntry;
 import de.exxcellent.challenge.model.WeatherDataEntry;
 
 import java.io.FileNotFoundException;
@@ -31,16 +34,22 @@ public final class App {
             );
 
             List<WeatherDataEntry> weatherDataEntryList = weatherReader.readData();
-
             WeatherAnalyzer weatherAnalyzer = new WeatherAnalyzer();
-
             int dayWithSmallestTempSpread = weatherAnalyzer.getDayWithMinTemperatureSpread(weatherDataEntryList);     // Your day analysis function call …
             System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-            String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+            IDataReader<FootballDataEntry> footballReader = new FootballReader(
+                    new CSVSourceConnector<FootballDataEntry>(
+                            Paths.get("./src/main/resources/de/exxcellent/challenge/football.csv")
+                    )
+            );
+            List<FootballDataEntry> footballDataEntryList = footballReader.readData();
+            FootballAnalyzer footballAnalyzer = new FootballAnalyzer();
+            String teamWithSmallestGoalSpread = footballAnalyzer.getTeamNameWithMinGoalDifference(footballDataEntryList); // Your goal analysis function call …
             System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
         }catch (FileNotFoundException e) {
-            System.out.println(e);
+            System.out.println("Could not find the specified files, please check the paths");
+            e.printStackTrace();
         }
 
     }
