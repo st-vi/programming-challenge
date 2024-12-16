1d0936c3498124c761213be4a5d90254637d841f
Stefan <svill33@gmail.com>
2024-12-16 18:09:24 +0100

    feat: add FootballAnalyzer skeleton

---
+++ src/main/java/de/exxcellent/challenge/analyzer/FootballAnalyzer.java
@@ -0,0 +1,12 @@
+package de.exxcellent.challenge.analyzer;
+
+import de.exxcellent.challenge.model.FootballDataEntry;
+import de.exxcellent.challenge.model.WeatherDataEntry;
+
+import java.util.List;
+
+public class FootballAnalyzer {
+    public String getTeamNameWithMinGoalDifference(List<FootballDataEntry> footballDataEntries){
+        return null;
+    }
+}
