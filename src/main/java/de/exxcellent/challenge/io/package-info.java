b6ddbff87a6bbf0e327cc558d93f79d8b9dda2b2
Stefan <svill33@gmail.com>
2024-12-16 18:34:29 +0100

    doc: add package-info.java for io package

---
+++ src/main/java/de/exxcellent/challenge/io/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Provides classes for reading data from external sources such as CSV files.
+ *
+ * This package includes data readers and connectors for reading football and weather data:
+ * - {@link de.exxcellent.challenge.io.WeatherReader} and {@link de.exxcellent.challenge.io.FootballReader}
+ *   are responsible for reading and mapping data from CSV files into objects like {@link de.exxcellent.challenge.model.WeatherDataEntry}
+ *   and {@link de.exxcellent.challenge.model.FootballDataEntry}.
+ * - {@link de.exxcellent.challenge.io.IDataReader} and {@link de.exxcellent.challenge.io.IDataSourceConnector}
+ *   define interfaces for reading data and connecting to data sources in a generic manner.
+ *
+ * These classes allow the application to import data, such as weather and football match statistics, for analysis.
+ *
+ * @since 1.0
+ */
+package de.exxcellent.challenge.io;
