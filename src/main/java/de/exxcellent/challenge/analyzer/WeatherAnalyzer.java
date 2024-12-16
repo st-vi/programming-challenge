3fb6c5430ca6cb621bbf26971129f0cafdde6f1b
Stefan <svill33@gmail.com>
2024-12-16 17:59:49 +0100

    feat: add functionality to WeatherAnalyzer skeleton

--- src/main/java/de/exxcellent/challenge/analyzer/WeatherAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/analyzer/WeatherAnalyzer.java
@@ -6,6 +6,19 @@ import java.util.List;
 
 public class WeatherAnalyzer {
     public int getDayWithMinTemperatureSpread(List<WeatherDataEntry> weatherDataEntries){
-        return 0;
+        if (weatherDataEntries.isEmpty()) {
+            throw new IllegalArgumentException("At least one WeatherDataEntry must be present for analyzing it.");
+        }
+        int minTempSpread = weatherDataEntries.get(0).maxTemp() - weatherDataEntries.get(0).minTemp();
+        int dayIdOfMinTempSpread = weatherDataEntries.get(0).day();
+        for(int i=1;i< weatherDataEntries.size();i++){
+            WeatherDataEntry weatherDataEntry = weatherDataEntries.get(i);
+            int currentTempSpread = weatherDataEntry.maxTemp() - weatherDataEntry.minTemp();
+            if(currentTempSpread < minTempSpread) {
+                minTempSpread = currentTempSpread;
+                dayIdOfMinTempSpread = weatherDataEntry.day();
+            }
+        }
+        return dayIdOfMinTempSpread;
     }
 }
