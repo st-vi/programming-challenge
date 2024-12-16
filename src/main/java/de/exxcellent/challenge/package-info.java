68480068043396762f81b96434ff4abe9358068f
Stefan <svill33@gmail.com>
2024-12-16 18:33:29 +0100

    doc: add package-info.java for root package

--- src/main/java/de/exxcellent/challenge/package-info.java
+++ src/main/java/de/exxcellent/challenge/package-info.java
@@ -1,8 +1,19 @@
 /**
- * The root package for the data challenge containing the entry point
- * {@link de.exxcellent.challenge.App} and containing the resource files.
- * Please create and document your own package structure appropriate to your design.
+ * Provides the core functionality for analyzing and processing weather and football data within the challenge application.
  *
- * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ * This package contains classes for reading, analyzing, and processing weather and football match statistics.
+ * The primary classes in this package include:
+ * - {@link de.exxcellent.challenge.analyzer.WeatherAnalyzer} and {@link de.exxcellent.challenge.analyzer.FootballAnalyzer}
+ *   for analyzing weather and football match data, respectively.
+ * - {@link de.exxcellent.challenge.io.WeatherReader} and {@link de.exxcellent.challenge.io.FootballReader}
+ *   for reading weather and football data from CSV files using data connectors.
+ * - {@link de.exxcellent.challenge.model.WeatherDataEntry} and {@link de.exxcellent.challenge.model.FootballDataEntry}
+ *   representing the data models for weather and football entries.
+ *
+ * The classes in this package are designed to work together to provide insights into football matches and weather data
+ * through various analysis methods, such as calculating the team with the minimum goal difference or identifying
+ * the day with the least temperature spread.
+ *
+ * @since 1.0
  */
 package de.exxcellent.challenge;
