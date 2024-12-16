e8918460be3a99544c7117f521baea2ce19d13b4
Stefan <svill33@gmail.com>
2024-12-16 16:12:47 +0100

    feat: add skeleton for main logic

---
+++ src/main/java/de/exxcellent/challenge/analyzer/WeatherAnalyzer.java
@@ -0,0 +1,11 @@
+package de.exxcellent.challenge.analyzer;
+
+import de.exxcellent.challenge.model.WeatherDataEntry;
+
+import java.util.List;
+
+public class WeatherAnalyzer {
+    public int getDayWithMinTemperatureSpread(List<WeatherDataEntry> weatherDataEntries){
+        return 0;
+    }
+}
