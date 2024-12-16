5badeba5900bdec4d5056273101edb6752b37304
Stefan <svill33@gmail.com>
2024-12-16 18:02:05 +0100

    doc: added javadoc for WeatherAnalyzer

--- src/main/java/de/exxcellent/challenge/analyzer/WeatherAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/analyzer/WeatherAnalyzer.java
@@ -4,7 +4,19 @@ import de.exxcellent.challenge.model.WeatherDataEntry;
 
 import java.util.List;
 
+/**
+ * A class responsible for analyzing weather data and providing various (currently only one) statistics.
+ */
 public class WeatherAnalyzer {
+
+    /**
+     * Returns the day with the minimum temperature spread (the difference between max and min temperatures).
+     * If there are multiple days with the same minimum temperature spread, the first one encountered in the list will be returned.
+     *
+     * @param weatherDataEntries A list of {@link WeatherDataEntry} objects containing weather data for each day.
+     * @return The day ID with the minimum temperature spread.
+     * @throws IllegalArgumentException if the list is empty.
+     */
     public int getDayWithMinTemperatureSpread(List<WeatherDataEntry> weatherDataEntries){
         if (weatherDataEntries.isEmpty()) {
             throw new IllegalArgumentException("At least one WeatherDataEntry must be present for analyzing it.");
