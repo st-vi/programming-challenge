1c841f1ab70b6acf0d57749e18ecfd1837d82473
Stefan <svill33@gmail.com>
2024-12-16 18:23:38 +0100

    feat: add functionality to FootballAnalyzer skeleton

--- src/main/java/de/exxcellent/challenge/analyzer/FootballAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/analyzer/FootballAnalyzer.java
@@ -7,6 +7,19 @@ import java.util.List;
 
 public class FootballAnalyzer {
     public String getTeamNameWithMinGoalDifference(List<FootballDataEntry> footballDataEntries){
-        return null;
+        if (footballDataEntries.isEmpty()) {
+            throw new IllegalArgumentException("At least one FootballDataEntry must be present for analyzing it.");
+        }
+        int minGoalDifference = Math.abs(footballDataEntries.get(0).numberGoals() - footballDataEntries.get(0).numberGoalsAllowed());
+        String teamName = footballDataEntries.get(0).teamName();
+        for(int i=1;i< footballDataEntries.size();i++){
+            FootballDataEntry footballDataEntry = footballDataEntries.get(i);
+            int currentGoalDifference = Math.abs(footballDataEntry.numberGoals() - footballDataEntry.numberGoalsAllowed());
+            if(currentGoalDifference < minGoalDifference) {
+                minGoalDifference = currentGoalDifference;
+                teamName = footballDataEntry.teamName();
+            }
+        }
+        return teamName;
     }
 }
--- src/main/java/de/exxcellent/challenge/io/FootballReader.java
+++ src/main/java/de/exxcellent/challenge/io/FootballReader.java
@@ -5,6 +5,8 @@ import de.exxcellent.challenge.model.WeatherDataEntry;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class FootballReader  implements IDataReader<FootballDataEntry> {
     public final IDataSourceConnector<FootballDataEntry> dataSourceConnector;
@@ -15,6 +17,16 @@ public class FootballReader  implements IDataReader<FootballDataEntry> {
 
     @Override
     public List<FootballDataEntry> readData() throws FileNotFoundException {
-        return null;
+        Function<Map<String, String>, FootballDataEntry> mapToFootballDataEntry = rawData -> {
+            String teamName = rawData.get("Team");
+            int goals = Integer.parseInt(rawData.get("Goals"));
+            int goalsAllowed = Integer.parseInt(rawData.get("Goals Allowed"));
+            return new FootballDataEntry(teamName, goals, goalsAllowed);
+        };
+        try {
+            return dataSourceConnector.readData(mapToFootballDataEntry);
+        }catch (NumberFormatException n){
+            throw new IllegalArgumentException("The provided data does not represent a FootballDataEntry");
+        }
     }
 }
