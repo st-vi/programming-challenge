2ad33e0b4766331dceb7c3f1752f47f7caf98f3b
Stefan <svill33@gmail.com>
2024-12-16 17:46:14 +0100

    feat: add unit test for successful weather analyses

---
+++ src/test/java/de/exxcellent/challenge/TestWeatherAnalyzer.java
@@ -0,0 +1,39 @@
+package de.exxcellent.challenge;
+
+import de.exxcellent.challenge.analyzer.WeatherAnalyzer;
+import de.exxcellent.challenge.io.WeatherReader;
+import de.exxcellent.challenge.model.WeatherDataEntry;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestWeatherAnalyzer {
+    @Test
+    void testSuccessGetDayWithMinTemperatureSpread() throws IOException {
+        List<Map<String,String>> mockData = new LinkedList<>();
+        Map<String,String> entry = new HashMap<>();
+        entry.put("Day", "1");
+        entry.put("MxT", "88");
+        entry.put("MnT", "59");
+        mockData.add(entry);
+        entry = new HashMap<>();
+        entry.put("Day", "2");
+        entry.put("MxT", "79");
+        entry.put("MnT", "63");
+        mockData.add(entry);
+
+        MockDataSourceConnector<WeatherDataEntry> mockDataSourceConnector = new MockDataSourceConnector<>(mockData);
+        WeatherReader weatherReader = new WeatherReader(mockDataSourceConnector);
+
+        List<WeatherDataEntry> weatherDataEntries = weatherReader.readData();
+        WeatherAnalyzer weatherAnalyzer = new WeatherAnalyzer();
+        int resultDayId = weatherAnalyzer.getDayWithMinTemperatureSpread(weatherDataEntries);
+        assertEquals(1, resultDayId);
+    }
+}
