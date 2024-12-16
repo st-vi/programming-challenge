1ba2c14b4f6fa804aa2642b905a85d702bf00122
Stefan <svill33@gmail.com>
2024-12-16 18:02:54 +0100

    doc: added javadoc for WeatherDataEntry

--- src/main/java/de/exxcellent/challenge/model/WeatherDataEntry.java
+++ src/main/java/de/exxcellent/challenge/model/WeatherDataEntry.java
@@ -1,4 +1,10 @@
 package de.exxcellent.challenge.model;
 
+
+/**
+ * A record representing weather data for a specific day.
+ * This record holds the day of the weather entry, the maximum temperature,
+ * and the minimum temperature for that day.
+ */
 public record WeatherDataEntry(int day, int maxTemp, int minTemp) {
 }
