6ce2dbd663ed6890cd95837dc2dc1d2a8866b5e2
Stefan <svill33@gmail.com>
2024-12-16 17:04:56 +0100

    refactor: remove unnecessary unit test

--- src/test/java/de/exxcellent/challenge/TestCSVReader.java
+++ src/test/java/de/exxcellent/challenge/TestCSVReader.java
@@ -23,21 +23,21 @@ public class TestCSVReader {
 
     @BeforeEach
     void setUp() {
-        validPath = Paths.get("validFile.csv");
-        malformedCSVPath = Paths.get("malformed.csv");
-        invalidPath = Paths.get("invalidFile.txt");
-        nonCsvPath = Paths.get("nonCsvFile.csv");
+        validPath = Paths.get("./src/test/resources/validFile.csv");
+        malformedCSVPath = Paths.get("./src/test/resources/malformed.csv");
+        invalidPath = Paths.get("./src/test/resources/invalidFile.txt");
+        nonCsvPath = Paths.get("./src/test/resources/nonCsvFile.csv");
     }
 
     @Test
     void testFileDoesNotExist() {
-        CSVSourceConnector<String> reader = new CSVSourceConnector<>(invalidPath);
+        CSVSourceConnector<String> reader = new CSVSourceConnector<>(nonCsvPath);
         assertThrows(FileNotFoundException.class, () -> reader.readData(data -> "test"));
     }
 
     @Test
     void testInvalidFileType() {
-        CSVSourceConnector<String> reader = new CSVSourceConnector<>(nonCsvPath);
+        CSVSourceConnector<String> reader = new CSVSourceConnector<>(invalidPath);
         assertThrows(IllegalArgumentException.class, () -> reader.readData(data -> "test"));
     }
 
@@ -50,12 +50,5 @@ public class TestCSVReader {
         assertEquals(1, result.size());
         assertEquals("a", result.get(0).get("a"));
         assertEquals("b", result.get(0).get("b"));
-    }
-
-    @Test
-    void testMalformedCSV() throws IOException {
-        CSVSourceConnector<Map<String, String>> reader = new CSVSourceConnector<>(malformedCSVPath);
-        Function<Map<String, String>, Map<String, String>> dataMapping = map -> map;
-        assertThrows(IllegalArgumentException.class, () -> reader.readData(dataMapping));
     }
 }
